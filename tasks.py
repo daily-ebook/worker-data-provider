da6d85abcfe748b349e181472f9bba252f4a61ac
Cristian Baldi <bld.cris.96@gmail.com>
2016-11-26 18:55:55 +0100

    Add support for multiple queues

--- tasks.py
+++ tasks.py
@@ -4,9 +4,14 @@ import mongoengine
 from ded.Recipe import Recipe
 from ded.DataProvider import DataProvider
 
-celery = Celery('tasks',
+celery = Celery('daily-ebook',
             broker="redis://redis:6379/0",
             backend="redis://redis:6379/0")
+celery.conf.task_routes = ([
+    ('ebook_generator.*', {'queue': 'eg'}),
+    ('data_provider.*', {'queue': 'dp'})
+],)
+
 # mongoengine.connect('celery_beat_daily_epub') useless for now
 """confs = {
     "CELERY_MONGODB_SCHEDULER_DB": "celery_beat_daily_epub",
@@ -15,7 +20,7 @@ celery = Celery('tasks',
 }
 celery.conf.update(confs)"""
 
-@celery.task(bind=True, name="tasks.get_sources_metadata")
+@celery.task(bind=True, name="data_provider.get_sources_metadata")
 def get_sources_metadata(self):
     # FIXME: improve this to only load modules one time
     data_provider = DataProvider()
@@ -25,7 +30,7 @@ def get_sources_metadata(self):
     return {'sources': metadatas}
 
 # bind=True to have the self parameter
-@celery.task(bind=True, name="tasks.generate_book_from_dict_recipe")
+@celery.task(bind=True, name="data_provider.generate_book_from_dict_recipe")
 def generate_book_from_dict_recipe(self, recipe_dict):
     self.update_state(state='PROGRESS', meta={'message': 'Starting generation'})
     recipe = Recipe.from_dict(recipe_dict)
@@ -33,6 +38,8 @@ def generate_book_from_dict_recipe(self, recipe_dict):
     recipe.build()
     self.update_state(state='PROGRESS', meta={'message': 'Rendering to HTML'})
     recipe.render()
-    return {'message': 'Book generation complete', 'html': recipe.html}
+    recipe_dict = recipe.to_dict()
+    render_task = celery.send_task('ebook_generator.render_recipe_to_ebook', args=[recipe_dict], kwargs={})
+    return {'message': 'Deferred execution to {0}'.format(render_task.id), 'task-id': render_task.id}
 
 
